b97a55bd9b0776ddad4cfaa89f1747fc098a7a44
Romero Rodriguez <cromero@se.veng.com.ar>
2023-07-15 19:10:57 -0300

    Se agregó clausula try-except

--- manejo_errores.py
+++ manejo_errores.py
@@ -1,7 +1,14 @@
 print("Hola CaC")
 
-print("División de 2 numeros enteros")
-numero1 = int(input("Ingrese un numero: "))
-numero2 = int(input("Ingrese un numero: "))
+print("\nDivisión de 2 numeros enteros")
+numero1 = int(input("Ingrese dividendo: "))
 
-print("El resultado del cociente es: ", numero1/numero2)
+while True:
+    # para gestion de excepciones tenemos las clausulas try/except
+    try:
+        numero2 = int(input("Ingrese divisor: "))
+        print("El resultado del cociente es: ", numero1/numero2)
+        break
+    except:
+        print("No es posible dividir por cero")
+

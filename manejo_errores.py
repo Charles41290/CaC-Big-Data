9f2ac8aad58c9cc81eca9ee4e727edc724cee91c
Romero Rodriguez <cromero@se.veng.com.ar>
2023-07-15 19:32:29 -0300

    Se gestionaron ZeroDivisionError/ValueError/ModuleNotFoundError

--- manejo_errores.py
+++ manejo_errores.py
@@ -1,14 +1,19 @@
 print("Hola CaC")
 
 print("\nDivisión de 2 numeros enteros")
-numero1 = int(input("Ingrese dividendo: "))
 
 while True:
     # para gestion de excepciones tenemos las clausulas try/except
     try:
+        numero1 = int(input("Ingrese dividendo: "))
         numero2 = int(input("Ingrese divisor: "))
         print("El resultado del cociente es: ", numero1/numero2)
         break
-    except:
+    except ZeroDivisionError:
         print("No es posible dividir por cero")
-
+    except ValueError:
+        print("Valor ingresado no válido. Ingrese un numero")
+try: 
+    import test.py
+except ModuleNotFoundError:
+    print("El modulo que se dea importar no existe")

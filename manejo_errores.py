340cafd899455e831b05b7f387530fdd379ba012
Romero Rodriguez <cromero@se.veng.com.ar>
2023-07-15 18:58:04 -0300

    Se ha empezado a codear codigo python

---
+++ manejo_errores.py
@@ -0,0 +1,7 @@
+print("Hola CaC")
+
+print("División de 2 numeros enteros")
+numero1 = int(input("Ingrese un numero: "))
+numero2 = int(input("Ingrese un numero: "))
+
+print("El resultado del cociente es: ", numero1/numero2)
